fdd66bb34a744990b9bb83d3d4e30e2954e770a5
Rafael muller <83794212+rafaelmuller01@users.noreply.github.com>
2021-07-14 19:04:38 -0300

    Update teste.dart

--- teste.dart
+++ teste.dart
@@ -1,18 +1,52 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
-void main() => runApp(Column(
-      children: const <Widget>[
-        Text(
-          'Deliver features faster',
-          textDirection: TextDirection.ltr,
+main(List<String> args) {
+  runApp(CalculadoraCombustivel());
+}
+
+class CalculadoraCombustivel extends StatelessWidget {
+  void calcularCombustivel() {
+    print('quase lá');
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Center(child: Text('calculadoracombustível')),
         ),
-        Text('seja bem vindo', textDirection: TextDirection.ltr),
-        Expanded(
-          child: FittedBox(
-            fit: BoxFit.contain, // otherwise the logo will be tiny
-            child: FlutterLogo(),
-          ),
+        bottomNavigationBar: BottomAppBar(
+          child: Text('app Desenvolvido pelos alunos'),
         ),
-      ],
-    ));
+        body: Padding(
+          padding: const EdgeInsets.all(40),
+          child: Column(children: [
+            Center(child: Text('bora economizar?')),
+            TextFormField(
+              decoration: InputDecoration(
+                  border: UnderlineInputBorder(),
+                  hintText: 'informe apenas valores númericos. Ex: 5.29',
+                  labelText: 'informe o valor da gasolina: '),
+            ),
+            TextFormField(
+              decoration: InputDecoration(
+                  border: UnderlineInputBorder(),
+                  hintText: 'informe apenas valores númericos. Ex: 4.29',
+                  labelText: 'informe o valor do alcool: '),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(40),
+              child: ElevatedButton(
+                onPressed: calcularCombustivel,
+                child: Text('calcular'),
+              ),
+            ),
+            Center(child: Text('o melhor combustível é: ')),
+          ]),
+        ),
+      ),
+    );
+  }
+}
